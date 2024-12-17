b9da5a9c6cf5e1ebdd6f92950e8a9e00127aaa09
Elijah Gutman <gutmane7@gmail.com>
2024-12-17 15:22:19 -0500

    init commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController
+  def index
+    response = HTTP
+      .headers("Authorization" => "Bearer #{ENV["NEWS_API_KEY"]}")
+      .get("https://newsapi.org/v2/everything?q=z#{params[:search_terms]}")
+    data = response.parse
+    articles = data["articles"]
+    render json: articles.uniq { |article| article["url"] }.map { |article| article.merge("id" => article["url"]) }
+  end
+end
